9019f7a6ebb40208c1ef5da73ec35bd196a74fde
TJ Pavaritpong <teetajp@gmail.com>
2023-01-18 16:17:19 -0800

    update post

--- posts/notion.ts
+++ posts/notion.ts
@@ -2,10 +2,10 @@ import { ExtendedRecordMap } from 'notion-types';
 import { getPageTitle } from 'notion-utils';
 
 export const POSTS = {
-  '2023-plans': {
-    date: new Date('2023-01-02').toDateString(),
-    uri: 'teetajp/Pushing-myself-to-new-limits-in-2023-2aeec7bf50fc4af39fe65c571eebf938'
-  },
+  // '2023-plans': {
+  //   date: new Date('2023-01-02').toDateString(),
+  //   uri: 'teetajp/Pushing-myself-to-new-limits-in-2023-2aeec7bf50fc4af39fe65c571eebf938'
+  // },
 };
 
 export interface PageInfo {
