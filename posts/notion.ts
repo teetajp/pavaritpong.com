1e49e98f2091b824fe96197f6eea212f956a7961
TJ Pavaritpong <teetajp@gmail.com>
2023-04-03 21:58:52 +0700

    reading list

--- posts/notion.ts
+++ posts/notion.ts
@@ -2,10 +2,10 @@ import { ExtendedRecordMap } from 'notion-types';
 import { getPageTitle } from 'notion-utils';
 
 export const POSTS = {
-//   '2023-plans': {
-//     date: new Date('2023-01-02').toDateString(),
-//     uri: 'teetajp/Pushing-myself-to-new-limits-in-2023-2aeec7bf50fc4af39fe65c571eebf938'
-//   },
+  'reading-list': {
+    date: new Date('2023-04-03').toDateString(),
+    uri: 'teetajp/Reading-List-5d66da34d0b5415e93f9bc9bdc4413d6'
+  },
 };
 
 export interface PageInfo {
