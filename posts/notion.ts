91c847d31cea8b682a80524895fdfbfed63dad0a
TJ Pavaritpong <teetajp@gmail.com>
2023-06-02 20:54:36 +0700

    update blog

--- posts/notion.ts
+++ posts/notion.ts
@@ -6,6 +6,10 @@ export const POSTS = {
   //  date: new Date('2023-05-18').toDateString(),
   //  uri: 'teetajp/2023-Reading-List-Book-Review-5d66da34d0b5415e93f9bc9bdc4413d6'
   // },
+  'how-to-turn-your-travels-into-a-documentary': {
+    date: new Date('2023-06-02').toDateString(),
+    uri: 'teetajp/How-To-Turn-Your-Travels-Into-a-Documentary-0dfbe70a88db4d55b58b41968f455440?pvs=4'
+  }
 };
 
 export interface PageInfo {
