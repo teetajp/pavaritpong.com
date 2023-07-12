2a79fcdaa16c73557d622a9bbce99264dd68347b
TJ Pavaritpong <teetajp@gmail.com>
2023-07-12 22:22:10 +0700

    truth serum

--- posts/notion.ts
+++ posts/notion.ts
@@ -2,10 +2,6 @@ import { ExtendedRecordMap } from 'notion-types';
 import { getPageTitle } from 'notion-utils';
 
 export const POSTS = {
-  // 'reading-list': {
-  //  date: new Date('2023-05-18').toDateString(),
-  //  uri: 'teetajp/2023-Reading-List-Book-Review-5d66da34d0b5415e93f9bc9bdc4413d6'
-  // },
   'how-to-turn-your-travels-into-a-documentary': {
     date: new Date('2023-06-02').toDateString(),
     uri: 'teetajp/How-To-Turn-Your-Travels-Into-a-Documentary-0dfbe70a88db4d55b58b41968f455440?pvs=4'
@@ -13,6 +9,10 @@ export const POSTS = {
   'redemption-poem-tj-pavaritpong': {
     date: new Date('2023-07-10').toDateString(),
     uri: 'https://teetajp.notion.site/Redemption-by-TJ-Pavaritpong-3e678100f50248e6843c7d30ab568f66'
+  },
+  'truth-serum-haunted-island-tj-pavaritpong': {
+    date: new Date('2023-07-12').toDateString(),
+    uri: 'https://teetajp.notion.site/Truth-Serum-Haunted-Island-by-TJ-Pavaritpong-43738fa3c7524e04a9466e47114d4c58?pvs=4'
   }
 };
 
