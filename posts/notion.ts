4b3997c2370015a56a8d8356c24fc572e5cb7486
TJ Pavaritpong <teetajp@gmail.com>
2023-08-24 20:59:02 -0500

    web

--- posts/notion.ts
+++ posts/notion.ts
@@ -6,10 +6,10 @@ export const POSTS = {
     date: new Date('2023-06-02').toDateString(),
     uri: 'teetajp/How-To-Turn-Your-Travels-Into-a-Documentary-0dfbe70a88db4d55b58b41968f455440?pvs=4'
   },
-  'redemption-poem-tj-pavaritpong': {
-    date: new Date('2023-07-10').toDateString(),
-    uri: 'https://teetajp.notion.site/Redemption-by-TJ-Pavaritpong-3e678100f50248e6843c7d30ab568f66?pvs=4'
-  },
+  // 'redemption-poem-tj-pavaritpong': {
+  //   date: new Date('2023-07-10').toDateString(),
+  //   uri: 'https://teetajp.notion.site/Redemption-by-TJ-Pavaritpong-3e678100f50248e6843c7d30ab568f66?pvs=4'
+  // },
   // 'truth-serum-haunted-island-tj-pavaritpong': {
   //   date: new Date('2023-07-12').toDateString(),
   //   uri: 'https://teetajp.notion.site/Truth-Serum-Haunted-Island-by-TJ-Pavaritpong-43738fa3c7524e04a9466e47114d4c58?pvs=4'
@@ -18,10 +18,10 @@ export const POSTS = {
   //   date: new Date('2023-07-13').toDateString(),
   //   uri: 'https://teetajp.notion.site/Ivy-Crown-McDojos-by-TJ-Pavaritpong-07af851c99394eb3830bd8a48b789817?pvs=4'
   // },
-  'lost-in-google-translation-by-tj-pavaritpong': {
-    date: new Date('2023-07-14').toDateString(),
-    uri: 'https://teetajp.notion.site/Lost-in-Google-Translation-by-TJ-Pavaritpong-4501649a661c4752a569259b95a1b2e1?pvs=4'
-  },
+  // 'lost-in-google-translation-by-tj-pavaritpong': {
+  //   date: new Date('2023-07-14').toDateString(),
+  //   uri: 'https://teetajp.notion.site/Lost-in-Google-Translation-by-TJ-Pavaritpong-4501649a661c4752a569259b95a1b2e1?pvs=4'
+  // },
 };
 
 export interface PageInfo {
